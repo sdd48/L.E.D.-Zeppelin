7b3b73aa7581ff96ea2b9807c70b79c60893723b
Aaron Wisner <daw268@cornell.edu>
2018-08-20 18:51:00 -0400

    Fix scotts failed fix of Aarons merge conflict

--- strips/test.py
+++ strips/test.py
@@ -1,10 +1,9 @@
-import strip
-import gui_strip
-import led_strip
+from strips.gui_strip import GuiStrip
+from strips.led_strip import LEDStrip
 import numpy as np
 
 #s = gui_strip.LEDStrip()
-s = gui_strip.GuiStrip()
+s = GuiStrip()
 test = np.random.randint(0, high=255, size=(150, 3))
 s.setStrip(test)
 s.update()
