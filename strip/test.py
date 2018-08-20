3a1cee19640711260ecf4e8ee8a88c7c5a2b24d3
Aaron Wisner <daw268@cornell.edu>
2018-08-19 21:54:16 -0400

    Add gui strip driver with tkinter

--- strip/test.py
+++ strip/test.py
@@ -1,6 +1,11 @@
-from strip import *
+import strip
+import gui_strip
 import numpy as np
-s = Strip()
+#s = strip.Strip()
+s = gui_strip.GuiStrip()
 test = np.random.randint(0, high=255, size=(150, 3))
 s.setStrip(test)
 s.update()
+
+while True:
+    s.rainbow(0.01)
