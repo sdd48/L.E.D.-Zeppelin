e5a5d997e603ddd40723704c774235d4a46ccd00
Aaron Wisner <daw268@cornell.edu>
2019-07-17 22:55:32 -0400

    Allow dynamic sizing of strips

--- wifinode/node/node.ino
+++ wifinode/node/node.ino
@@ -10,26 +10,28 @@
 #define PORT 5120
 #define MDNS_NAME "LEDZeppelin"
 
-#define NUM_LEDS 300
-#define BUF_SIZE (NUM_LEDS*3)
+#define MAX_LEDS 600
+#define BUF_SIZE (MAX_LEDS*3)
 
 // Watchdog timeout
 #define WDT_MS 2000
 
-uint8_t incoming[BUF_SIZE];
+struct UpdatePacket {
+  uint32_t numLeds;
+  uint8_t rgb[MAX_LEDS][3];
+};
 
-const char RESP_OKAY[] = "OKAY";
-const char RESP_FAIL[] = "FAIL";
-
+UpdatePacket *incoming = nullptr; //heap alloc
+Adafruit_NeoPixel *strip = nullptr;
 WiFiUDP Udp;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 
 void setup() {
+  // Heap alloc our buffer first
+  incoming = new UpdatePacket();
   // put your setup code here, to run once:
   ESP.wdtEnable(WDT_MS);
   Serial.begin(115200);
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
 
   WiFi.begin(SSID, PASS);
   Serial.print("Connecting");
@@ -54,33 +56,42 @@ void setup() {
 void loop() {
   ESP.wdtFeed();
   MDNS.update();
-  // We keep reading until there is nothing left
+  
+  if (recvPacket(*incoming)) {
+    // receive incoming UDP packets
+    // If the packet size is incorrect, we need to create a new strip object
+    if (!strip || strip->numPixels() != incoming->numLeds) {
+      delete strip;
+      strip = new Adafruit_NeoPixel(incoming->numLeds, LED_PIN, NEO_GRB + NEO_KHZ800);
+      strip->begin();
+//    }
+//    Serial.println(ESP.getFreeHeap());
+//    Serial.println(ESP.getMaxFreeBlockSize());
+//    Serial.println(ESP.getHeapFragmentation());
+
+    for (int i=0; i < MAX_LEDS; i++) {
+      strip->setPixelColor(i, incoming->rgb[i][0], incoming->rgb[i][1], incoming->rgb[i][2]);
+    }
+    strip->show();
+  }
+
+}
+
+bool recvPacket(UpdatePacket &packet) {
   int packetSize = 0;
   int len = 0;
   do {
     packetSize = Udp.parsePacket();
-    if (packetSize) {
-      len = Udp.read(incoming, BUF_SIZE);
-      Serial.println("Got packet");
+    if (packetSize > BUF_SIZE) { // Not enough room
+      Serial.println("Too large of packet recieved");
+    } else if (packetSize) {
+      len = Udp.read(packet.rgb[0], BUF_SIZE);
+      //Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+      //int len = Udp.read(incoming, BUF_SIZE);
+      //Serial.printf("UDP packet contents: %s\n", incoming);
+      packet.numLeds = len / 3;
     }
   } while (packetSize);
-  
 
-  if (len == BUF_SIZE) //got Update
-  {
-    // receive incoming UDP packets
-    //Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
-    //int len = Udp.read(incoming, BUF_SIZE);
-    //Serial.printf("UDP packet contents: %s\n", incoming);
-    Serial.println(ESP.getFreeHeap());
-    Serial.println(ESP.getMaxFreeBlockSize());
-    Serial.println(ESP.getHeapFragmentation());
-    delay(1);
-    for (int i=0; i < NUM_LEDS; i++) {
-      strip.setPixelColor(i, incoming[3*i], incoming[3*i+1], incoming[3*i+2]);
-    }
-    strip.show();
-  }
-  yield();
-
+  return len;
 }
