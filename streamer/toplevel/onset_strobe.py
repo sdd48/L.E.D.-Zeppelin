138e94d7fcb891f2e3a6a4d94a43386bf8fa4d7a
Scott Dickson <sdd48@cornell.edu>
2019-08-02 13:13:50 -0400

    added wheel to onset strobe

--- streamer/toplevel/onset_strobe.py
+++ streamer/toplevel/onset_strobe.py
@@ -5,23 +5,28 @@ import collections
 
 from streamer.streamer import Streamer
 from streamer.onset_streamer import OnsetStreamer
+from streamer.wheel_streamer import WheelStreamer
 
 class OnsetStrobe(Streamer):
 	def __init__(self, numleds, fsample, frame_width):
 		super(OnsetStrobe, self).__init__()
 		self.numleds = numleds
 		self.onset = OnsetStreamer(fsample, frame_width)
+		self.wheel = WheelStreamer(numleds)
 
 	def input(self, data):
+		self.wheel.input(data)
 		self.onset.input(data)
 
 
 	def outputReady(self):
-		return self.onset.outputReady()
+		return self.onset.outputReady() and self.wheel.outputReady()
 
 	def output(self):
 		val = self.onset.output()
+		strip = self.wheel.output()
+
 		if val > 0.5:
-			return 255*np.ones((self.numleds, 3))
+			return strip
 		else:
 			return np.zeros((self.numleds, 3))
